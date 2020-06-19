38907d5e124c0e6d56244291a5a94943d303d001
Hugh <det.nosnibor00@gmail.com>
2020-06-19 10:37:50 -0500

    fetch now works after adding this to the backend server:
    let allowCrossDomain = function(req, res, next) {
      res.header('Access-Control-Allow-Origin', "*");
      res.header('Access-Control-Allow-Headers', "*");
      next();
    }
    app.use(allowCrossDomain);

--- src/App.js
+++ src/App.js
@@ -2,25 +2,49 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Email extends React.Component {
+  render() {
+    return (
+      <div className="email">
+
+      </div>
+    );
+  }
+}
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emailsFromServer: {},
+      fetchingAllEmails: false,
+    }
+  }
+
+  //run the fetch as soon as app loads to access emails faster for the user
+  async componentDidMount() {
+    const response = await fetch('http://localhost:3001/emails')
+    const json = await response.json()
+    this.setState({ emailsFromServer: json })
+    console.log(this.state.emailsFromServer)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        </header>
+        <main className="main">
+          {this.state.fetchingAllEmails ?
+            //this.spinner()
+            <p>Loading...</p> :
+            <p>Emails fetched</p>
+          }
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
